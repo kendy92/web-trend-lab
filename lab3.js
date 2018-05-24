64ef8878573f5dbb09731d720c519c8d9a506159
kendy92 <dinhconganh@gmail.com>
2018-05-24 10:28:12 -0400

    add part 2 of lab 3

--- lab3.js
+++ lab3.js
@@ -1,9 +1,50 @@
-/* PART 1 */
+//define global variable
 var map;
+var addresses = [];
+var mapMarkers = [];
 var Toronto = { lat: 43, lng: -79.38 };
+//class
+var MapMarker = /** @class */ (function () {
+    //add constructor function
+    function MapMarker(address, latlng) {
+        this.Address = address;
+        this.LatLng = latlng;
+    }
+    return MapMarker;
+}());
+//making ajax request to server
+$.ajax({
+    dataType: 'json',
+    url: 'locations.json',
+    success: function (data) {
+        addresses = data;
+        for (var _i = 0, addresses_1 = addresses; _i < addresses_1.length; _i++) {
+            var i = addresses_1[_i];
+            //add map marker to array of map markers
+            var newLatLng = { lat: i.lat, lng: i.lon };
+            var newMapMarker = new MapMarker(i.address, newLatLng);
+            mapMarkers.push(newMapMarker);
+        }
+    }
+});
+function addMarker() {
+    //loop through map markers
+    for (var _i = 0, mapMarkers_1 = mapMarkers; _i < mapMarkers_1.length; _i++) {
+        var i = mapMarkers_1[_i];
+        //assign latlng to new variable
+        var places = { lat: i.LatLng.lat, lng: i.LatLng.lng };
+        //create map markers
+        var marker = new google.maps.Marker({
+            position: places,
+            map: map,
+            title: i.address
+        });
+    }
+}
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: Toronto,
         zoom: 8
     });
+    setTimeout(addMarker, 200);
 }
--- lab3.ts
+++ lab3.ts
@@ -1,6 +1,8 @@
-/* PART 1 */
+//define global variable
 let map : any;
-
+let addresses : any[] = [];
+let mapMarkers: MapMarker[] = [];
+let Toronto: LatLng = {lat: 43, lng: -79.38}
 
 //interface
 interface LatLng{
@@ -8,8 +10,50 @@ interface LatLng{
   lng: number
 }
 
-let Toronto: LatLng = {lat: 43, lng: -79.38}
+//class
+class MapMarker{
+  Address: string;
+  LatLng: LatLng;
 
+  //add constructor function
+  public constructor(address: string, latlng: LatLng) {
+    this.Address = address;
+    this.LatLng = latlng;
+  }
+}
+
+
+
+//making ajax request to server
+$.ajax({
+  dataType: 'json',
+  url: 'locations.json',
+  success: function(data){
+
+    addresses = data;
+    for(let i of addresses) {
+      //add map marker to array of map markers
+      let newLatLng: LatLng = {lat: i.lat, lng: i.lon};
+      let newMapMarker: MapMarker = new MapMarker(i.address, newLatLng);
+      mapMarkers.push(newMapMarker);
+    }
+  }
+});
+
+function addMarker() {
+  //loop through map markers
+  for(let i of mapMarkers) {
+
+    //assign latlng to new variable
+    let places:LatLng = {lat: i.LatLng.lat, lng: i.LatLng.lng};
+    //create map markers
+    var marker = new google.maps.Marker({
+    position: places,
+    map: map,
+    title: i.address
+  });
+  }
+}
 
 function initMap() {
   map = new google.maps.Map (
@@ -19,4 +63,5 @@ function initMap() {
       zoom: 8
     }
   );
+  setTimeout(addMarker,200);
 }
